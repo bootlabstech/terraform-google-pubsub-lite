04d39e9eb29f2429bb5996c998e3461b7dfd8d88
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-05-30 16:21:19 +0530

    added the dynamic block

--- main.tf
+++ main.tf
@@ -1,40 +1,32 @@
 resource "google_pubsub_lite_reservation" "example" {
-  name                 = var.reservation
-  project              = var.project_id
-  throughput_capacity  = var.throughput_capacity
-  region               = var.region
-}
-
-resource "google_pubsub_lite_subscription" "example" {
-  name    = var.subscription
-  project = var.project_id
-  topic   = var.topic
-  zone    = var.zone
-  region  = var.region
-  delivery_config {
-    delivery_requirement = var.delivery_requirement
-  }
-  depends_on = [
-    google_pubsub_lite_topic.example
-  ]
+  name                = var.reservation
+  project             = var.project_id
+  throughput_capacity = var.throughput_capacity
+  region              = var.region
 }
 
 resource "google_pubsub_lite_topic" "example" {
-  name     = var.topic
-  zone     = var.zone
-  region   = var.region
-  project  = var.project_id
+  name    = var.topic
+  zone    = var.zone
+  region  = var.region
+  project = var.project_id
 
-  partition_config {
-    count = var.count
-    capacity {
-      publish_mib_per_sec   = var.publish_mib_per_sec
-      subscribe_mib_per_sec = var.subscribe_mib_per_sec
+  dynamic "partition_config" {
+    for_each = var.partition_config_count
+    content {
+      count                   = partition_config.value.partition_config_count
+      capacity {
+        publish_mib_per_sec   = partition_config.value.capacity.publish_mib_per_sec
+        subscribe_mib_per_sec = partition_config.value.capacity.subscribe_mib_per_sec
+      }
     }
   }
-
-  retention_config {
-    per_partition_bytes = var.per_partition_bytes
+  
+  dynamic "retention_config" {
+    for_each = var.per_partition_bytes 
+    content {
+     per_partition_bytes = retention_config.value.per_partition_bytes 
+    }
   }
 
   reservation_config {
@@ -45,6 +37,21 @@ resource "google_pubsub_lite_topic" "example" {
   ]
 }
 
-
-
+resource "google_pubsub_lite_subscription" "example" {
+  project = var.project_id
+  name   = var.subscription
+  topic  = var.topic
+  zone   = var.zone
+  region = var.region
+  
+ dynamic "delivery_config" {
+    for_each = var.delivery_requirement
+    content {
+      delivery_requirement = delivery_config.value.delivery_requirement
+    }
+  }
+  depends_on = [
+    google_pubsub_lite_topic.example
+  ]
+}
 
